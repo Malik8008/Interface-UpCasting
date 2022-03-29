4dc2b9c85e937a7d7104e50a151c856a53549bfa
Malik8008 <tu60prbs1@code.edu.az>
2022-03-29 12:20:54 +0400

    Praktika Task completed

---
+++ Praktika-Task-2/Praktika-Task-2/Group.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Praktika_Task_2
+{
+    internal class Group:Student
+    {
+        private static string fullname;
+        private static string groupno;
+        private static int avgpoint;
+        public int No;
+        public Student[] students;
+        public int Limit;
+
+
+        public Group(int no, Student Students, int limit) : base(fullname, groupno, avgpoint)
+        {
+
+            No = no;
+            Fullname = Students.Fullname;
+            Limit = limit;
+
+
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"no: {No}, Student: {Fullname}, Limit: {Limit}");
+        }
+
+
+
+        public Student[] AddStudent(Student student)
+        {
+            Array.Resize(ref students, students.Length + 1);
+            students[students.Length - 1] = student;
+
+            return students;
+
+        }
+    }
+}
---
+++ Praktika-Task-2/Praktika-Task-2/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Praktika_Task_2
+{
+    internal class Student
+    {
+        public string Fullname;
+        public string GroupNo;
+        public int AvgPoint;
+
+        public Student(string fullname, string groupno, int avgpoint)
+        {
+            Fullname = fullname;
+            GroupNo = groupno;
+            AvgPoint = avgpoint;
+        }
+    }
+}
